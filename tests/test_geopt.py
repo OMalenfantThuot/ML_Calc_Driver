3397d3f891e68e2a900b36a99842ac87b50e04e0
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2019-10-16 10:08:33 -0400

    isclose

--- tests/test_geopt.py
+++ tests/test_geopt.py
@@ -29,7 +29,7 @@ class TestGeopt:
     def test_H2O(self):
         init = Job(posinp=self.posH2O, calculator=self.calcH2O)
         init.run("energy")
-        assert init.results["energy"][0] == -2076.7576
+        assert np.isclose(init.results["energy"][0], -2076.7576, atol=1e-4)
         geo = Geopt(posinp=self.posH2O, calculator=self.calcH2O)
         geo.run()
         assert 0.964 < geo.final_posinp.distance(0,1) < 0.965
@@ -37,4 +37,4 @@ class TestGeopt:
         #TODO Check the angle
         final = Job(posinp=geo.final_posinp, calculator=self.calcH2O)
         final.run("energy")
-        assert final.results["energy"][0] == -2078.6301
+        assert np.isclose(final.results["energy"][0], -2078.6301, atol=1e-4)
