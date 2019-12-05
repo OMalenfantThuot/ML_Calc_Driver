a03977a740a88d0e6aad835c61280f5d4b51fb89
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2019-12-05 13:44:19 -0500

    version 0.2.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["numpy>=1.16"]
 
 setup(
     name="mlcalcdriver",
-    version="0.2.2",
+    version="0.2.3",
     author="Olivier Malenfant-Thuot",
     author_email="malenfantthuotolivier@gmail.com",
     description="A package to drive atomic calculations using machine learned models.",
