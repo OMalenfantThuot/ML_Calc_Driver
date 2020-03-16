e6549bb6bc5894d45ed55b45f2c063e04f64c879
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2020-03-16 16:05:26 -0400

    temp commit - calculator object should be all set, phonons workflow still need some work

--- mlcalcdriver/calculators/schnetpack.py
+++ mlcalcdriver/calculators/schnetpack.py
@@ -51,40 +51,12 @@ class SchnetPackCalculator(Calculator):
         Main method to use when making a calculation with
         the calculator.
         """
-        # if property not in self.available_properties:
-        #    if derivative:
-        #        if property == "forces":
-        #            if "energy" in self.available_properties:
-        #                init_property, deriv_name, out_name = (
-        #                    "energy",
-        #                    "forces",
-        #                    "forces",
-        #                )
-        #            else:
-        #                raise ValueError(
-        #                    "This model can't be used for forces predictions."
-        #                )
-        #        else:
-        #            raise NotImplementedError(
-        #                "Derivatives of other quantities than the energy are not implemented yet."
-        #            )
-        #    else:
-        #        raise ValueError(
-        #            "The property {} is not in the available properties of the model : {}.".format(
-        #                property, self.available_properties
-        #            )
-        #        )
-        # elif property == "energy" and "energy_U0" in self.available_properties:
-        #    init_property, out_name = "energy_U0", "energy"
-        # else:
-        #    init_property, out_name = property, property
-
         if property not in self.available_properties:
             if property == "forces" and "energy" in self.available_properties:
-                init_property, out_name, derivative = "energy", "forces", 1
+                init_property, out_name, derivative = "energy", "forces", -1
                 wrt = ["_positions"]
             elif property == "hessian" and "energy" in self.available_properties:
-                init_property, out_name, derivative = "energy", "hessian", 2
+                init_property, out_name, derivative = "energy", "hessian", -2
                 wrt = ["_positions", "_positions"]
             elif property == "hessian" and forces in self.available_properties:
                 init_property, out_name, derivative = "forces", "hessian", 1
@@ -125,14 +97,18 @@ class SchnetPackCalculator(Calculator):
                     for batch in data_loader:
                         batch = {k: v.to(device) for k, v in batch.items()}
                         pred.append(self.model(batch))
-        if derivative == 1:
+        if abs(derivative) == 1:
             for batch in data_loader:
                 batch = {k: v.to(device) for k, v in batch.items()}
                 batch[wrt[0]].requires_grad_()
                 results = self.model(batch)
-                deriv1 = torch_derivative(results[init_property], batch[wrt[0]])
+                deriv1 = torch.unsqueeze(
+                    torch_derivative(results[init_property], batch[wrt[0]]), 0
+                )
+                if derivative < 0:
+                    deriv1 = -1.0 * deriv1
                 pred.append({out_name: deriv1})
-        if derivative == 2:
+        if abs(derivative) == 2:
             for batch in data_loader:
                 batch = {k: v.to(device) for k, v in batch.items()}
                 for inp in set(wrt):
@@ -144,42 +120,13 @@ class SchnetPackCalculator(Calculator):
                     ),
                     batch[wrt[0]],
                 )
-                pred.append({out_name: d2rdx2})
-            # if derivative == 2:
-            #    hessian = np.zeros([9, 9])
-            #    print(drdx)
-            #    print(drdx.shape)
-            #    print(batch["_positions"])
-            #    k=0
-            #    for i in range(drdx.shape[1]):
-            #        for j in range(drdx.shape[2]):
-            #            print(drdx[0,i,j])
-            #            drdx2= torch.autograd.grad(
-            #                    -1.0 * drdx[0, i, j],
-            #                    batch["_positions"],
-            #                    grad_outputs=torch.ones_like(drdx[0,i,j]),
-            #                    create_graph=True,
-            #                    retain_graph=True,
-            #                )[0]
-            #            print(drdx2)
-            #            print(k)
-            #            hessian[k] = drdx2.detach().numpy().flatten()
-            #            k += 1
-            #    hessian = hessian * EV_TO_HA * B_TO_ANG **2
-            #    hessian = (hessian + hessian.T)/2
-            #    print(np.around(hessian, decimals=2))
+                if derivative < 0:
+                    deriv2 = -1.0 * deriv2
+                pred.append({out_name: deriv2})
         predictions = {}
         predictions[property] = np.concatenate(
             [batch[out_name].cpu().detach().numpy() for batch in pred]
         )
-        # if derivative:
-        #    predictions[out_name] = np.concatenate(
-        #        [batch[deriv_name].cpu().detach().numpy() for batch in pred]
-        #    )
-        # else:
-        #    predictions[out_name] = np.concatenate(
-        #        [batch[init_property].cpu().detach().numpy() for batch in pred]
-        #    )
         return predictions
 
     def _get_available_properties(self):
@@ -201,6 +148,7 @@ class SchnetPackCalculator(Calculator):
 
     def _get_representation_type(self):
         r"""
+        Private method to determine representation type (schnet or wcasf).
         """
         if "representation.cutoff.cutoff" in self.model.state_dict().keys():
             self.model_type = "wacsf"
