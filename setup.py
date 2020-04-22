2a8d3abe3dd59635ce03b7b70ef51a1d8c7f4a40
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2020-04-22 13:38:29 -0400

    travis with latest schnet version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["numpy>=1.18", "torch>=1.4.0", "schnetpack==0.3"]
+requirements = ["numpy>=1.18", "torch>=1.4.0", "schnetpack>=0.3"]
 
 setup(
     name="mlcalcdriver",
