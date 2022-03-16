ab906269c4059dcc37eb6ec2815e184ae2146fad
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2022-03-16 16:20:52 -0400

    removed old import

--- mlcalcdriver/calculators/__init__.py
+++ mlcalcdriver/calculators/__init__.py
@@ -10,4 +10,3 @@ from .calculator import Calculator
 from .schnetpack import SchnetPackCalculator
 from .ensemble import Ensemble, EnsembleCalculator, AseEnsembleCalculator
 from .ase_calculators.asespkcalculator import AseSpkCalculator
-from .schnetpack_lowmem import LowMemSPCalculator
