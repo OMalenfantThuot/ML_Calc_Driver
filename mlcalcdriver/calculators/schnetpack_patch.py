various fixes, forces should be all good, hessian still needs work

mlcalcdriver/calculators/schnetpack_patch.py
@@ -90,6 +90,9 @@ class PatchSPCalculator(SchnetPackCalculator):
         init_property, out_name, derivative, wrt = get_derivative_names(
             property, self.available_properties
         )
+        if abs(derivative) >= 1:
+            self.model.output_modules[0].create_graph = True
+
         pbc = True if atoms.pbc.any() else False
         environment_provider = (
             AseEnvironmentProvider(cutoff=self.cutoff)
@@ -152,6 +155,7 @@ class PatchSPCalculator(SchnetPackCalculator):
                     for i, patch in enumerate(results)
                 ]
             )
+
         elif property == "forces":
             forces = np.zeros((len(atoms), 3))
             for i in range(len(results)):
@@ -163,8 +167,31 @@ class PatchSPCalculator(SchnetPackCalculator):
                     .numpy()[subcells_main_idx[i]]
                 )
             predictions["forces"] = forces
+
         elif property == "hessian":
-            print(results[0]["hessian"].shape)
+            hessian = np.zeros((3 * len(atoms), 3 * len(atoms)))
+
+            for i in range(len(results)):
+
+                (
+                    hessian_original_cell_idx_0,
+                    hessian_original_cell_idx_1,
+                ) = prepare_hessian_indices(original_cell_idx[i])
+
+                (
+                    hessian_subcells_main_idx_0,
+                    hessian_subcells_main_idx_1,
+                ) = prepare_hessian_indices(subcells_main_idx[i])
+
+                hessian[hessian_original_cell_idx_0, hessian_original_cell_idx_1] = (
+                    results[i]["hessian"]
+                    .detach()
+                    .squeeze()
+                    .cpu()
+                    .numpy()[hessian_subcells_main_idx_0, hessian_subcells_main_idx_1]
+                )
+            predictions["hessian"] = hessian
+
         else:
             raise NotImplementedError()
 
@@ -196,3 +223,10 @@ class PatchSPCalculator(SchnetPackCalculator):
 
         patches_model = PatchesAtomisticModel(self.model.representation, patches_output)
         self.model = patches_model.to(self.device)
+
+
+def prepare_hessian_indices(input_idx):
+    bias = np.tile(np.array([0, 1, 2]), len(input_idx))
+    hessian_idx = np.repeat(3 * input_idx, 3) + bias
+    idx_0, idx_1 = np.meshgrid(hessian_idx, hessian_idx, indexing="ij")
+    return idx_0, idx_1

mlcalcdriver/interfaces/atoms_to_patches.py
@@ -1,5 +1,5 @@
 from ase.geometry import Cell
-from ase import Atom
+from ase import Atom, Atoms
 from collections.abc import Sequence
 import numpy as np
 from copy import deepcopy
@@ -56,9 +56,21 @@ class AtomsToPatches:
 
     def split_atoms(self, atoms):
 
+        # Fix for atoms exactly on the cell frontier
+        new_scaled_positions = np.round(atoms.get_scaled_positions(), decimals=8)
+        t_idx_0, t_idx_1 = np.where(
+            np.isclose(new_scaled_positions, 1.0, rtol=0, atol=1e-8)
+        )
+        new_scaled_positions[t_idx_0, t_idx_1] -= 1.0
+
+        new_pbc = []
+        for i in range(3):
+            new_pbc.append(True if atoms.pbc[i] and self.grid[i] == 1 else False)
+
+        atoms = Atoms(symbols=atoms.symbols, cell=atoms.cell, pbc=new_pbc)
+        atoms.set_scaled_positions(new_scaled_positions)
+
         # Define grid and cells
-        atoms = deepcopy(atoms)
-        atoms.set_pbc(self.grid == 1)
         full_cell = atoms.cell
         grid_cell = Cell(full_cell / np.broadcast_to(self.grid, (3, 3)).T)
 
@@ -67,7 +79,8 @@ class AtomsToPatches:
             np.radians(min(full_cell.angles()))
         )
         full_scaled_buffer_length = buffer_length / full_cell.cellpar()[:3]
-        full_scaled_buffer_length[np.where(atoms.pbc)[0]] = 0
+        full_scaled_buffer_length[np.where(self.grid == 1)[0]] = 0
+
         if np.any(full_scaled_buffer_length >= 0.5):
             raise ValueError("The supercell is too small to use with this buffer.")
         grid_scaled_buffer_length = full_scaled_buffer_length * self.grid
@@ -93,7 +106,7 @@ class AtomsToPatches:
 
         # Create subcells as atoms instances
         subcell_as_atoms_list = []
-        main_atoms_idx_list = []
+        main_subcell_idx_list = []
         original_atoms_idx_list = []
 
         for i, subcell in enumerate(subcells_idx):
@@ -123,10 +136,8 @@ class AtomsToPatches:
                 )
             )[0]
 
-            subcell_as_atoms_list.append(
-                deepcopy(buffered_atoms[buffered_subcell_atoms_idx])
-            )
-            main_atoms_idx_list.append(main_subcell_idx)
+            subcell_as_atoms_list.append(buffered_atoms[buffered_subcell_atoms_idx])
+            main_subcell_idx_list.append(main_subcell_idx)
             original_atoms_idx_list.append(buffered_subcell_atoms_idx[main_subcell_idx])
 
         # Returns:
@@ -136,7 +147,7 @@ class AtomsToPatches:
         # 3) a list of the original index of the atoms
         #    to map back per atom predicted properties
         #    to the original configuration.
-        return subcell_as_atoms_list, main_atoms_idx_list, original_atoms_idx_list
+        return subcell_as_atoms_list, main_subcell_idx_list, original_atoms_idx_list
 
 
 def add_initial_buffer(atoms, scaled_buffer_length, full_cell):