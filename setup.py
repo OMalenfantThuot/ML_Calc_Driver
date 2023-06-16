5c36646bceac88da6dc8219327d494cf739f86ca
Olivier Malenfant-Thuot <malenfantthuotolivier@gmail.com>
2023-06-16 14:59:33 -0400

    update requirements

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
 requirements = [
-    "numpy>=1.20",
-    "torch>=1.9.0",
-    "schnetpack>=1.0",
+    "numpy>=1.20<1.24",
+    "torch>=2.0.1",
+    "schnetpack==1.0.1",
     "ase>=3.22.0",
 ]
 
 setup(
     name="mlcalcdriver",
-    version="1.4.0",
+    version="1.4.1",
     author="Olivier Malenfant-Thuot",
     author_email="malenfantthuotolivier@gmail.com",
     description="A package to drive atomic calculations using machine learned models.",
@@ -22,7 +22,6 @@ setup(
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
